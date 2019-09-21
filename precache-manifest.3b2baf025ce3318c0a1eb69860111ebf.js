4100226334cd12cd146265605e3660a37d7110ec
Jesulen Vicente <54116236+Jesulen@users.noreply.github.com>
2019-09-21 17:11:09 -0300

    Add files via upload
    Commit de fim de workshop

---
+++ precache-manifest.3b2baf025ce3318c0a1eb69860111ebf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2bdd3000b9f471b1e4d4775530af90d",
+    "url": "JesulenVicente.github.io/index.html"
+  },
+  {
+    "revision": "be2d52d30ffe9cba6c75",
+    "url": "JesulenVicente.github.io/static/css/main.ecbc8c1f.chunk.css"
+  },
+  {
+    "revision": "9b964ac4bf37818ddff2",
+    "url": "JesulenVicente.github.io/static/js/2.e3ecdd7b.chunk.js"
+  },
+  {
+    "revision": "be2d52d30ffe9cba6c75",
+    "url": "JesulenVicente.github.io/static/js/main.290b40ec.chunk.js"
+  },
+  {
+    "revision": "d3920af6ee800c2634b8",
+    "url": "JesulenVicente.github.io/static/js/runtime-main.da7e6b6b.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "JesulenVicente.github.io/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "affed25811305530fd1cd2b9eb9e8e5c",
+    "url": "JesulenVicente.github.io/static/media/sun.affed258.jpg"
+  }
+]);
